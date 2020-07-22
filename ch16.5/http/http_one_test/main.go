ee859c75b10a077f5616ddd9dd35a5646fb5eaf1
magiclz233 <magiclz233@163.com>
2020-07-22 23:52:29 +0800

    go http hello world test

---
+++ ch16.5/http/http_one_test/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err.Error())
+	}
+}
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("调用HelloServer!")
+	fmt.Fprintf(w, "Hello,"+req.URL.Path[1:])
+}
