0668721fcbde0e886505930b277bf32874f44ae4
magiclz233 <magiclz233@163.com>
2020-07-26 23:21:47 +0800

    go flag test

--- ch15.5/flag_test/flag_two_test/main.go
+++ ch15.5/flag_test/flag_two_test/main.go
@@ -1,1 +1,29 @@
-package flag_two_test
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+func main() {
+	var name string
+	var age int
+	var married bool
+	var delay time.Duration
+
+	flag.StringVar(&name, "name", "张三", "姓名")
+	flag.IntVar(&age, "age", 18, "年龄")
+	flag.BoolVar(&married, "married", false, "婚否")
+	flag.DurationVar(&delay, "d", 0, "延迟的时间间隔")
+
+	// 解析命令行参数
+	flag.Parse()
+	fmt.Println(name, age, married, delay)
+	//返回命令行参数后的其他参数
+	fmt.Println(flag.Args())
+	//返回命令行参数后的其他参数个数
+	fmt.Println(flag.NArg())
+	//返回使用的命令行参数个数
+	fmt.Println(flag.NFlag())
+}
