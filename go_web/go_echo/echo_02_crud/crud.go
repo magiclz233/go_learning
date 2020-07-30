aecee8f2ec126d717cb8d4c8863b77c83d60470e
magic <magiclz233@163.com>
2020-07-30 16:08:15 +0800

    go echo crud

---
+++ go_web/go_echo/echo_02_crud/crud.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strconv"
+)
+
+var users = []User{
+	{Id: 1,
+		Name: "magic",
+		Age:  23,
+		Sex:  "男",
+	},
+	{Id: 2,
+		Name: "uzi",
+		Age:  23,
+		Sex:  "男",
+	},
+	{Id: 3,
+		Name: "faker",
+		Age:  24,
+		Sex:  "女",
+	},
+}
+
+func getUser(c echo.Context) error {
+	// User ID 来自于url "users/:id"
+	id, _ := strconv.Atoi(c.Param("id"))
+	for _, user := range users {
+		if id == user.Id {
+			return c.JSON(http.StatusOK, user)
+		}
+	}
+	return c.String(404, "没有找到id:"+strconv.Itoa(id)+"对应的user")
+}
+
+func saveUser(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users = append(users, *u)
+	return c.JSON(http.StatusOK, users)
+}
+
+func updateUser(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	for _, user := range users {
+		if id == user.Id {
+			user = *u
+			return c.JSON(http.StatusOK, user)
+		}
+	}
+	return c.String(404, "没有找到该id: "+strconv.Itoa(id)+" 对应的用户")
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	for i, user := range users {
+		if id == user.Id {
+			users = append(users[:i], users[i+1:]...)
+		}
+	}
+	return c.JSON(http.StatusOK, users)
+}
---
+++ go_web/go_echo/echo_02_crud/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/labstack/echo/v4"
+
+func main() {
+	e := echo.New()
+	e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+	e.Logger.Fatal(e.Start(":8080"))
+}
