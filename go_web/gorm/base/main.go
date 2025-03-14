cc8972721b5d20d4e1e6367e9f4f09972af4912c
luozhang <luozhang@wafersystems.com>
2025-03-14 23:17:53 +0800

    gorm修改

--- go_web/gorm/base/main.go
+++ go_web/gorm/base/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -16,17 +18,38 @@ func main() {
 	if err != nil {
 		panic("无法链接sqlite!")
 	}
+
+	// 自动迁移
 	db.AutoMigrate(&Product{})
-	db.Create(&Product{Code: "D42", Price: 100})
+	fmt.Println("数据库表创建成功")
+
+	// 创建记录
+	newProduct := Product{Code: "D42", Price: 100}
+	db.Create(&newProduct)
+	fmt.Printf("创建商品记录: Code=%s, Price=%d\n", newProduct.Code, newProduct.Price)
+
+	// 查询记录
 	var product Product
 	db.First(&product, 1)
-	db.First(&product, "code = ?", "D42") // find product with code D42
-	// 更新数据 - update product's price to 200
-	db.Model(&product).Update("Price", 200)
-	// 更新数据 - update multiple fields
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	fmt.Printf("查询ID=1的商品: Code=%s, Price=%d\n", product.Code, product.Price)
 
-	// 删除数据 - delete product
+	db.First(&product, "code = ?", "D42")
+	fmt.Printf("查询Code=D42的商品: ID=%d, Price=%d\n", product.ID, product.Price)
+
+	// 更新单个字段
+	db.Model(&product).Update("Price", 200)
+	fmt.Printf("更新后的价格: Price=%d\n", product.Price)
+
+	// 更新多个字段
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
+	fmt.Printf("批量更新后: Code=%s, Price=%d\n", product.Code, product.Price)
+
+	// 删除记录
 	db.Delete(&product, 1)
+	fmt.Println("删除ID=1的商品记录")
+
+	// 验证删除
+	var count int64
+	db.Model(&Product{}).Count(&count)
+	fmt.Printf("当前数据库中商品总数: %d\n", count)
 }
