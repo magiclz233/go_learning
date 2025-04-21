b1a01a72c7d5ecdec4db0a75574c4b140eaeb7b8
magiclz233 <magiclz233@163.com>
2025-04-21 22:44:16 +0800

    feat: 添加从不同源读取数据的函数
    新增 `ReadFrom` 函数，支持从标准输入、文件和字符串中读取数据。该函数通过 `io.Reader` 接口实现通用性，便于扩展和重用。

--- go_base/ch14_io_file/file_copy_test/main.go
+++ go_base/ch14_io_file/file_copy_test/main.go
@@ -10,6 +10,12 @@ import (
 func main() {
 
 	//CopyFile()
+	// 从标准输入读取
+	// data, err = ReadFrom(os.Stdin, 11)
+	// 从文件读取
+	// data1, err = ReadFrom(os.File, 11)
+	// 从字符串读取
+// data, err = ReadFrom(strings.NewReader("from string"), 12)
 }
 
 func CopyFile(oldName, newName string) (written int64, err error) {
@@ -27,3 +33,14 @@ func CopyFile(oldName, newName string) (written int64, err error) {
 
 	return io.Copy(newFile, oldFile)
 }
+
+func ReadFrom(reader io.Reader, num int) ([]byte, error) {
+	p := make([]byte, num)
+	n, err := reader.Read(p)
+	if n>0 {
+		return p[:n], nil
+	}
+	return p, err
+}
+
+
